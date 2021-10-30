2ba710d596da31b4cc32b6094c15781f282bfdf1
iulian <iulian.rusu999@gmail.com>
2021-10-30 23:00:39 +0300

    Add OpenAPI documentation to routes

--- book-library/src/main/kotlin/com/pos/booklibrary/controllers/AuthorController.kt
+++ book-library/src/main/kotlin/com/pos/booklibrary/controllers/AuthorController.kt
@@ -3,6 +3,9 @@ package com.pos.booklibrary.controllers
 import com.pos.booklibrary.persistence.query.AuthorQueryCriteria
 import com.pos.booklibrary.models.Author
 import com.pos.booklibrary.services.AuthorAccessService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -12,19 +15,45 @@ class AuthorController {
     @Autowired
     private lateinit var authorAccessService: AuthorAccessService
 
+    @Operation(summary = "Get all authors")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Authors retrieved")
+    ])
     @GetMapping("/authors")
     fun getAllAuthors(@RequestParam params: Map<String, String>) =
         authorAccessService.getAllAuthors(AuthorQueryCriteria(params))
 
+    @Operation(summary = "Get author specified by ID")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Author retrieved"),
+        ApiResponse(responseCode = "404", description = "Author not found")
+    ])
     @GetMapping("/authors/{id}")
     fun getAuthor(@PathVariable id: Long) = authorAccessService.getAuthor(id)
 
+    @Operation(summary = "Create a single author")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "Author created"),
+        ApiResponse(responseCode = "406", description = "Missing or invalid author data")
+    ])
     @PostMapping("/authors")
     fun postAuthor(@RequestBody newAuthor: Author) = authorAccessService.postAuthor(newAuthor)
 
+    @Operation(summary = "Update or create a single author")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "204", description = "Author updated"),
+        ApiResponse(responseCode = "201", description = "Author created"),
+        ApiResponse(responseCode = "406", description = "Missing required author data"),
+        ApiResponse(responseCode = "409", description = "Error when inserting author data"),
+    ])
     @PutMapping("/authors/{id}")
     fun putAuthor(@PathVariable id: Long, @RequestBody newAuthor: Author) = authorAccessService.putAuthor(id, newAuthor)
 
+    @Operation(summary = "Delete a single author")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "204", description = "Author deleted"),
+        ApiResponse(responseCode = "404", description = "Author not found")
+    ])
     @DeleteMapping("/authors/{id}")
     fun deleteAuthor(@PathVariable id: Long) = authorAccessService.deleteAuthor(id)
 }
--- book-library/src/main/kotlin/com/pos/booklibrary/controllers/BookController.kt
+++ book-library/src/main/kotlin/com/pos/booklibrary/controllers/BookController.kt
@@ -3,6 +3,9 @@ package com.pos.booklibrary.controllers
 import com.pos.booklibrary.persistence.query.BookQueryCriteria
 import com.pos.booklibrary.models.Book
 import com.pos.booklibrary.services.BookAccessService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -12,37 +15,86 @@ class BookController {
     @Autowired
     private lateinit var bookAccessService: BookAccessService
 
+    @Operation(summary = "Get all books")
+    @ApiResponses(value = [ApiResponse(responseCode = "200", description = "Retrieved book list")])
     @GetMapping("/books")
     fun getAllBooks(@RequestParam params: Map<String, String>) =
         bookAccessService.getAllBooks(BookQueryCriteria(params))
 
+    @Operation(summary = "Get a single book")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Book retrieved"),
+        ApiResponse(responseCode = "404", description = "Book not found")
+    ])
     @GetMapping("/books/{isbn}")
     fun getBook(@PathVariable isbn: String) = bookAccessService.getBook(isbn)
 
+    @Operation(summary = "Create a single book")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "Book created"),
+        ApiResponse(responseCode = "406", description = "Missing or invalid book data")
+    ])
     @PostMapping("/books")
     fun postBook(@RequestBody newBook: Book) = bookAccessService.postBook(newBook)
 
+    @Operation(summary = "Update or create a single book")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "204", description = "Book updated"),
+        ApiResponse(responseCode = "201", description = "Book created"),
+        ApiResponse(responseCode = "406", description = "Missing required book data"),
+        ApiResponse(responseCode = "409", description = "Error when inserting book data"),
+    ])
     @PutMapping("/books/{isbn}")
     fun putBook(@PathVariable isbn: String, @RequestBody newBook: Book) = bookAccessService.putBook(isbn, newBook)
 
+    @Operation(summary = "Delete a single book")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "204", description = "Book deleted"),
+        ApiResponse(responseCode = "404", description = "Book not found")
+    ])
     @DeleteMapping("/books/{isbn}")
     fun deleteBook(@PathVariable isbn: String) = bookAccessService.deleteBook(isbn)
 
+    @Operation(summary = "Get the book's author list")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Authors retrieved")
+    ])
     @GetMapping("/books/{isbn}/authors")
     fun getBookAuthors(@PathVariable isbn: String) = bookAccessService.getBookAuthors(isbn)
 
+    @Operation(summary = "Get book author with specified index")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Author retrieved"),
+        ApiResponse(responseCode = "404", description = "Author or book does not exist")
+    ])
     @GetMapping("/books/{isbn}/authors/{index}")
     fun getBookAuthor(@PathVariable isbn: String, @PathVariable index: Long) =
         bookAccessService.getBookAuthor(isbn, index)
 
+    @Operation(summary = "Create or update the list of author IDs of a specified book. " +
+            "The author's position in the array determines the index")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "202", description = "Book's author list created or updated"),
+        ApiResponse(responseCode = "406", description = "Invalid author data"),
+    ])
     @PostMapping("/books/{isbn}/authors")
     fun postBookAuthors(@PathVariable isbn: String, @RequestBody bookAuthorIds: List<Long>) =
         bookAccessService.postBookAuthors(isbn, bookAuthorIds)
 
+    @Operation(summary = "Delete author at specified index from a book")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "202", description = "Author deleted"),
+        ApiResponse(responseCode = "404", description = "Book or author not found"),
+    ])
     @DeleteMapping("/books/{isbn}/authors/{index}")
     fun deleteBookAuthor(@PathVariable isbn: String, @PathVariable index: Long) =
         bookAccessService.deleteBookAuthor(isbn, index)
 
+    @Operation(summary = "Delete all authors from a book, if any exist")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "202", description = "Authors deleted"),
+        ApiResponse(responseCode = "404", description = "Book not found"),
+    ])
     @DeleteMapping("/books/{isbn}/authors")
     fun deleteBookAuthors(@PathVariable isbn: String) =
         bookAccessService.deleteBookAuthors(isbn)
